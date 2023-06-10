2277aa23692c1e37c33c35133e176d51636daf8f
xu-cell <2459811340@qq.com>
2023-06-10 15:32:17 +0800

    完成lab1:3

--- user/primes.c
+++ user/primes.c
@@ -2,35 +2,36 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-// 
+// 当我第一个线程是筛
+
+//
+
 
 void re_pipe(int pipe_id) {
     int p[2]; 
     pipe(p);
     // 读取左边的线程管道的第一个质数
     int first_value;
-    read(pipe_id,&first_value,sizeof(int));
-    printf("prime %d\n",first_value);
-        
-
-    // 读取左边
-    int value;
+    int n = read(pipe_id,&first_value,sizeof(int));
+    if(n > 0) {printf("prime %d\n",first_value);}
+    else exit(0);
+    
+    int value;   
     while(read(pipe_id,&value,sizeof(int))) {
-        if(value % first_value) {
+        if(value % first_value != 0) {
             write(p[1],&value,sizeof(int));
         }
     }
+    close(p[1]);
+     //printf("-----log----\n");
     int pid = fork();
-    if(pid > 0) {
+    if(pid > 0) {    
        wait(&pid);
                
     } else {
+        //printf("创建一个线程tid = %d\n",getpid());
         re_pipe(p[0]);
     }
-          
-    close(p[1]);
-    close(pipe_id);
-    wait(0);
     exit(0);
 }
 int main(int argc, char *argv[]) {
@@ -38,11 +39,12 @@ int main(int argc, char *argv[]) {
     pipe(p1);
     for(int i = 2;i <= 35;i++) {
         write(p1[1],&i,sizeof(int));
-    }
-    
+    } 
+    close(p1[1]);
     int pid = fork();
     if(pid > 0) {
-        wait(&pid);
+     
+    wait(&pid);
     } else {
         re_pipe(p1[0]); // 传入左边线程的读端
     }
